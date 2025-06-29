14239aed9fe4630489d6e4c2e678337c7f9c5706
Veras-D <dveras2310@gmail.com>
2025-06-29 01:15:26 -0300

    fix(dotenv): Add dotenv load in main() on test envioroment

--- Backend/src/test/java/com/veras/mythOrFactLGBT/MythOrFactLgbtApplicationTests.java
+++ Backend/src/test/java/com/veras/mythOrFactLGBT/MythOrFactLgbtApplicationTests.java
@@ -2,8 +2,10 @@ package com.veras.mythOrFactLGBT;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "dotenv.path=./.env")
 class MythOrFactLgbtApplicationTests {
 
 	@Test
