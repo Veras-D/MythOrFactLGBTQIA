6c94074360a6a819e800e7961ce47230bb4d3473
Veras-D <dveras2310@gmail.com>
2025-07-02 16:50:18 -0300

    feat(security): Add cors config

--- Backend/src/main/java/com/veras/mythOrFactLGBT/config/SecurityConfig.java
+++ Backend/src/main/java/com/veras/mythOrFactLGBT/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://myth-or-fact-lgbtqia.vercel.app")); // Allow your frontend origin
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://myth-or-fact-lgbtqia.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
