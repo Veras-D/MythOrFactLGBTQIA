c2432fc787f3048ea1e6a52c4fb8981e7e8dfbc4
Veras-D <dveras2310@gmail.com>
2025-07-03 06:39:32 -0300

    test(userController): Update userController test

--- Backend/src/test/java/com/veras/mythOrFactLGBT/controller/GameHistoryControllerTest.java
+++ Backend/src/test/java/com/veras/mythOrFactLGBT/controller/GameHistoryControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -36,8 +37,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 
-
-@SpringBootTest
+@WebMvcTest(GameHistoryController.class)
 @AutoConfigureMockMvc
 // @ActiveProfiles("test")
 class GameHistoryControllerTest {
--- Backend/src/test/java/com/veras/mythOrFactLGBT/controller/StatementControllerTest.java
+++ Backend/src/test/java/com/veras/mythOrFactLGBT/controller/StatementControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -31,7 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 
-@SpringBootTest
+@WebMvcTest(StatementController.class)
 @AutoConfigureMockMvc
 class StatementControllerTest {
 
